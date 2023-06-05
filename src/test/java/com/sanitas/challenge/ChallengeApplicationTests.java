62ffabd25691eba37b236499b01a69ce2557b6fc
Joaquina <jooadisarro@gmail.com>
2023-06-05 15:27:42 +0200

    project initialization

---
+++ src/test/java/com/sanitas/challenge/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanitas.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
