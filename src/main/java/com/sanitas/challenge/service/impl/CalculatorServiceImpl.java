ff90010e6b2a81a7dbbd85f8322aa12665762e1b
Joaquina <jooadisarro@gmail.com>
2023-06-05 16:53:53 +0200

    added tracer functionality in service

--- src/main/java/com/sanitas/challenge/service/impl/CalculatorServiceImpl.java
+++ src/main/java/com/sanitas/challenge/service/impl/CalculatorServiceImpl.java
@@ -2,34 +2,45 @@ package com.sanitas.challenge.service.impl;
 
 import com.sanitas.challenge.domain.OperationValues;
 import com.sanitas.challenge.service.ICalculatorService;
-import lombok.RequiredArgsConstructor;
+import io.corp.calculator.TracerImpl;
 import org.springframework.stereotype.Service;
 
 /**
  * Service implementation of CalculatorService.
  */
 @Service
-@RequiredArgsConstructor
 public class CalculatorServiceImpl implements ICalculatorService {
+
+    private final TracerImpl tracer;
+
+    public CalculatorServiceImpl() {
+        this.tracer = new TracerImpl();
+    }
 
     /**
      * Process that adds two numbers.
+     *
      * @param operation with the values.
      * @return result value.
      */
     @Override
     public Double add(OperationValues operation) {
-        return operation.getValueA() + operation.getValueB();
+        Double result = operation.getValueA() + operation.getValueB();
+        tracer.trace(result);
+        return result;
     }
 
     /**
      * Endpoint that subtracts two numbers.
+     *
      * @param operation with the values.
      * @return result value.
      */
     @Override
     public Double subtract(OperationValues operation) {
-        return operation.getValueA() - operation.getValueB();
+        Double result = operation.getValueA() - operation.getValueB();
+        tracer.trace(result);
+        return result;
     }
 
 }
